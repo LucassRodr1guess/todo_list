e37ed3e38798225338c7a94f7c8ca68f06146f04
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2022-05-23 10:10:03 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 //scripts de conexão
 const conn = async()=>{
-    const atlas = await mongoose.connect('monodb+srv://userLR:logineregistro@fiaptecnico.solkl.mongodb.net/dblr')
+    const atlas = await mongoose.connect('mongodb+srv://userLR:logineregistro@fiaptecnico.solkl.mongodb.net/dblr')
 }
 
 //exportar as infomações para acesso externo
